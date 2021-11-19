70b9c7fa5e4a4c791861272749a351326e75c3d2
NataliaPW <pavlushkina1980@yandex.ru>
2021-11-19 23:45:09 +0300

    add button menulist

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function () {
+  
+ $('.menulist__btn').on('click', function () {
+    $('.menulist').toggleClass('menulist--active');
+    $('.menulist__list').toggleClass('menulist__list--active');
+    $('.menulist__btn').toggleClass('menulist__btn--active');
+    $('.menulist__link').toggleClass('menulist__link--active');
+$('.btn__icon-arrow').addClass('btn__icon-arrow--active');
+
+ })
+
+  $('.menulist__link').on('click', function () {
+    $('.menulist').removeClass('menulist--active');
+    $('.menulist__list').removeClass('menulist__list--active');
+    $('.menulist__btn').removeClass('menulist__btn--active');
+    $('.menulist__link').addClass('menulist__link--active');
+    $('.btn__icon-arrow').removeClass('btn__icon-arrow--active');
+
+  });
 
 });
