7c72c0e18621194147d9fc40d2e5e4da2301f563
NataliaPW <pavlushkina1980@yandex.ru>
2021-11-22 00:54:34 +0300

    rec menu list add slider, footer, brands

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 $(function () {
-  
- $('.menulist__btn').on('click', function () {
+
+  $('.menulist__btn').on('click', function () {
     $('.menulist').toggleClass('menulist--active');
     $('.menulist__list').toggleClass('menulist__list--active');
     $('.menulist__btn').toggleClass('menulist__btn--active');
     $('.menulist__link').toggleClass('menulist__link--active');
-$('.btn__icon-arrow').addClass('btn__icon-arrow--active');
+    $('.btn__icon-arrow').addClass('btn__icon-arrow--active');
 
- })
+  })
 
   $('.menulist__link').on('click', function () {
     $('.menulist').removeClass('menulist--active');
@@ -16,6 +16,17 @@ $('.btn__icon-arrow').addClass('btn__icon-arrow--active');
     $('.menulist__link').addClass('menulist__link--active');
     $('.btn__icon-arrow').removeClass('btn__icon-arrow--active');
 
+  });
+
+  $('.header__slaider').slick({
+    nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+
+    prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+
+    infinite: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpead: 2000
   });
 
 });
