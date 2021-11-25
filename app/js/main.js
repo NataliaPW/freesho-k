a47838a6e286323555dcc8c9cad771c1eda7b3eb
NataliaPW <pavlushkina1980@yandex.ru>
2021-11-25 03:11:07 +0300

    add card, rem slaider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
 $(function () {
 
+  /*подключение лист меню - выпадашка*/
+
   $('.menulist__btn').on('click', function () {
     $('.menulist').toggleClass('menulist--active');
     $('.menulist__list').toggleClass('menulist__list--active');
@@ -15,8 +17,9 @@ $(function () {
     $('.menulist__btn').removeClass('menulist__btn--active');
     $('.menulist__link').addClass('menulist__link--active');
     $('.btn__icon-arrow').removeClass('btn__icon-arrow--active');
-
   });
+
+  /*подключение слайдера*/
 
   $('.header__slaider').slick({
     nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
@@ -26,7 +29,45 @@ $(function () {
     infinite: false,
     fade: true,
     autoplay: true,
-    autoplaySpead: 2000
+    autoplaySpead: 2000,
   });
+
+  /*анимация сердца*/
+  
+$('.card__btn-heart').on('click', function() {
+   $(this).toggleClass('card__btn-heart--active');
+});
+
+/*счетчик товара*/
+
+$('.card__pic-btn--minus').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('.card__pic').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+ 
+    $input.val(value);
+});
+ 
+$('.card__pic-btn--plus').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('.card__pic').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value < 100) {
+        value = value + 1;
+    } else {
+        value =100;
+    }
+ 
+    $input.val(value);
+});
 
 });
