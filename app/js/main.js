753db3f310eec35da092003b601ff56e2baf50ec
NataliaPW <pavlushkina1980@yandex.ru>
2021-11-27 17:07:12 +0300

    home work 12

--- app/js/main.js
+++ app/js/main.js
@@ -32,7 +32,7 @@ $(function () {
     autoplaySpead: 2000,
   });
 
-  /*анимация сердца*/
+ /*анимация сердца*/
 
   $('.card__btn-heart').on('click', function () {
     $(this).toggleClass('card__btn-heart--active');
@@ -81,28 +81,28 @@ $(function () {
   var Mixer = mixitup('.top__content');
 
   /*кнопка показать еще*/
-  /* size_li = $("#myList li").size();
-      x=3;
-      $('#myList li:lt('+x+')').show();
-      $('#loadMore').click(function () {
-          x= (x+5 <= size_li) ? x+5 : size_li;
-          $('#myList li:lt('+x+')').show();
-      });
-      $('#showLess').click(function () {
-          x=(x-5<0) ? 3 : x-5;
-          $('#myList li').not(':lt('+x+')').hide();
-      }); */
-
+/* size_li = $("#myList li").size();
+    x=3;
+    $('#myList li:lt('+x+')').show();
+    $('#loadMore').click(function () {
+        x= (x+5 <= size_li) ? x+5 : size_li;
+        $('#myList li:lt('+x+')').show();
+    });
+    $('#showLess').click(function () {
+        x=(x-5<0) ? 3 : x-5;
+        $('#myList li').not(':lt('+x+')').hide();
+    }); */
+  
 
   /*добавление товара в корзину*/
+ 
 
-
-
+  
 
 });
 /*?  http://jsfiddle.net/cse_tushar/6FzSb/*/
 /* $(document).ready(function () { */
-/* $(function () {
+  /* $(function () {
     size_li = $("#myList li").size();
     x=3;
     $('#myList li:lt('+x+')').show();
